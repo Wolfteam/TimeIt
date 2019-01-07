1be88431e4eb670aa27d770a8bcac0f3f54f255f
wolfteam20 <mimo4325@gmail.com>
2019-01-06 23:08:35 -0400

    [TimeIt] Added some converters

---
+++ TimeIt/TimeIt/Converters/StringToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TimeIt.Converters
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string hexColor = (string)value;
+            return Color.FromHex(hexColor);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
