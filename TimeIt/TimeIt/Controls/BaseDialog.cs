713b24e05303e97245ae24f8624b9b50267f24e1
wolfteam20 <mimo4325@gmail.com>
2019-01-20 23:59:38 -0400

    Added a base dialog control

---
+++ TimeIt/TimeIt/Controls/BaseDialog.cs
@@ -0,0 +1,40 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System.Threading.Tasks;
+using TimeIt.Interfaces;
+using Xamarin.Forms;
+
+namespace TimeIt.Controls
+{
+    public class BaseDialog<T> : PopupPage
+    {
+        // the awaitable task
+        public Task<T> PageClosedTask
+            => PageClosedTaskCompletionSource.Task;
+
+        // the task completion source
+        public TaskCompletionSource<T> PageClosedTaskCompletionSource { get; set; }
+
+        public BaseDialog(IConfirmationDialog contentBody)
+        {
+            Content = contentBody as View;
+
+            // init the task completion source
+            PageClosedTaskCompletionSource = new TaskCompletionSource<T>();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // Prevent back button pressed action on android (return true)
+            (Content as IConfirmationDialog).OnOptionSelected?.Invoke(false);
+            return base.OnBackButtonPressed();
+        }
+
+        // Invoced when background is clicked
+        protected override bool OnBackgroundClicked()
+        {
+            // Prevent background clicked action(return false)
+            (Content as IConfirmationDialog).OnOptionSelected?.Invoke(false);
+            return base.OnBackgroundClicked();
+        }
+    }
+}
