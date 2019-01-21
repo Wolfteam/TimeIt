a68ecf1e6dc808df270f3d077dbaa6494438529b
wolfteam20 <mimo4325@gmail.com>
2019-01-20 23:53:12 -0400

    Added some enums that will ne used by the messenger class

---
+++ TimeIt/TimeIt/Enums/MessageType.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeIt.Enums
+{
+    public enum MessageType
+    {
+        ADD_TIMER,
+        EDIT_TIMER,
+        ADD_INTERVAL,
+        EDIT_INTERVAL,
+        INTERVAL_ADDED_EDITED
+    }
+}
