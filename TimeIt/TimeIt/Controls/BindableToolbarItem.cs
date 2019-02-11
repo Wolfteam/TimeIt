07bdf74a47fbd90e4c53d80302b98622edea421e
wolfteam20 <mimo4325@gmail.com>
2019-02-10 23:38:57 -0400

    [TimeIt] Added a custom toolbar item that can be hidden. Added a constant class

---
+++ TimeIt/TimeIt/Controls/BindableToolbarItem.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+
+namespace TimeIt.Controls
+{
+    public class BindableToolbarItem : ToolbarItem
+    {
+        public static readonly BindableProperty IsVisibleProperty = BindableProperty.Create(nameof(IsVisible), typeof(bool), typeof(BindableToolbarItem), true, BindingMode.TwoWay, propertyChanged: OnIsVisibleChanged);
+
+        public bool IsVisible
+        {
+            get => (bool)GetValue(IsVisibleProperty);
+            set => SetValue(IsVisibleProperty, value);
+        }
+
+        private static void OnIsVisibleChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var item = bindable as BindableToolbarItem;
+
+            if (item == null || item.Parent == null)
+                return;
+
+            var toolbarItems = ((ContentPage)item.Parent).ToolbarItems;
+
+            if ((bool)newvalue && !toolbarItems.Contains(item))
+            {
+                Device.BeginInvokeOnMainThread(() => { toolbarItems.Add(item); });
+            }
+            else if (!(bool)newvalue && toolbarItems.Contains(item))
+            {
+                Device.BeginInvokeOnMainThread(() => { toolbarItems.Remove(item); });
+            }
+        }
+    }
+}
---
+++ TimeIt/TimeIt/Helpers/Constans.cs
@@ -0,0 +1,7 @@
+namespace TimeIt.Helpers
+{
+    public class Constans
+    {
+        public const string DefaultTimeSpanFormat = "hh\\:mm\\:ss";
+    }
+}
