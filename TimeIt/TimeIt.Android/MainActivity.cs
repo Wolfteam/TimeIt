219bbf4396c2f761de0acc7f2cdf347d14af5dcf
wolfteam20 <mimo4325@gmail.com>
2019-02-13 23:45:31 -0400

    [TimeIt.Android] Updated icon launcher. Added / Updated some nugets

--- TimeIt/TimeIt.Android/MainActivity.cs
+++ TimeIt/TimeIt.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using PanCardView.Droid;
 
 namespace TimeIt.Droid
 {
-    [Activity(Label = "TimeIt", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TimeIt", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- TimeIt/TimeIt.Android/Resources/Resource.designer.cs
+++ TimeIt/TimeIt.Android/Resources/Resource.designer.cs
@@ -7719,13 +7719,7 @@ namespace TimeIt.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int icon = 2130903040;
-			
-			// aapt resource value: 0x7f030001
-			public const int icon_round = 2130903041;
-			
-			// aapt resource value: 0x7f030002
-			public const int launcher_foreground = 2130903042;
+			public const int ic_launcher = 2130903040;
 			
 			static Mipmap()
 			{
