ef364927dbfd4f9d83d89d76031bf37939a57607
wolfteam20 <mimo4325@gmail.com>
2019-06-07 21:59:07 -0400

    [TimeIt] Added a sound helper

---
+++ TimeIt/TimeIt/Helpers/SoundHelper.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.IO;
+
+namespace TimeIt.Helpers
+{
+    public class SoundHelper
+    {
+        public static void PlaySound(string filename, double volume = 100)
+        {
+            try
+            {
+                var player = CrossSimpleAudioPlayer.Current;
+                player.Volume = volume;
+                player.Load(filename);
+                player.Play();
+            }
+            catch (Exception)
+            {
+                //well
+            }
+        }
+
+        public static void PlaySound(Stream stream, double volume = 100)
+        {
+            try
+            {
+                var player = CrossSimpleAudioPlayer.Current;
+                player.Volume = volume;
+                player.Load(stream);
+                player.Play();
+            }
+            catch (Exception)
+            {
+                //well
+            }
+        }
+    }
+}
