0b8b17a20be3d3e542cd55487d82f536dc6fb60b
wolfteam20 <mimo4325@gmail.com>
2019-06-07 22:06:04 -0400

    [TimeIt] Reworked the custom timer logic

--- TimeIt/TimeIt/Helpers/CustomTimer.cs
+++ TimeIt/TimeIt/Helpers/CustomTimer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace TimeIt.Helpers
@@ -7,11 +9,12 @@ namespace TimeIt.Helpers
     public class CustomTimer
     {
         private readonly TimeSpan _timeSpan;
-        private readonly Action _callback;
+        private volatile Action _callback;
+        private volatile Stopwatch _stopwatch = new Stopwatch();
 
-        private static CancellationTokenSource _cancellationTokenSource;
+        private volatile CancellationTokenSource _cancellationTokenSource;
 
-        public bool IsRunning { get; set; }
+        public bool IsRunning { get; private set; }
         public bool IsPaused { get; set; }
 
         public CustomTimer(TimeSpan timeSpan, Action callback)
@@ -24,22 +27,40 @@ namespace TimeIt.Helpers
         public void Start()
         {
             IsRunning = true;
-            CancellationTokenSource cts = _cancellationTokenSource; // safe copy
-            Device.StartTimer(_timeSpan, () =>
+            _stopwatch.Start();
+            Device.BeginInvokeOnMainThread(async () =>
             {
-                if (cts.IsCancellationRequested)
+                while (!_cancellationTokenSource.IsCancellationRequested)
                 {
-                    return false;
+                    _callback.Invoke();
+                    //kinda hack...
+                    int ms = (int)(_timeSpan.TotalMilliseconds - _stopwatch.Elapsed.TotalMilliseconds);
+                    if (ms > 0)
+                        await Task.Delay(ms);
+                    _stopwatch.Restart();
                 }
-                _callback.Invoke();
-                return true; //true to continuous, false to single use
+                _cancellationTokenSource = new CancellationTokenSource();
+                _stopwatch.Stop();
             });
+            //Task.Run(async () =>
+            //{
+            //    while (!_cancellationTokenSource.IsCancellationRequested)
+            //    {
+            //        _callback.Invoke();
+            //        //kinda hack...
+            //        int ms = (int)(_timeSpan.TotalMilliseconds - _stopwatch.Elapsed.TotalMilliseconds);
+            //        await Task.Delay(ms);
+            //        _stopwatch.Restart();
+            //    }
+            //    _cancellationTokenSource = new CancellationTokenSource();
+            //    _stopwatch.Stop();
+            //});
         }
 
         public void Stop()
         {
             IsRunning = false;
-            Interlocked.Exchange(ref _cancellationTokenSource, new CancellationTokenSource()).Cancel();
+            _cancellationTokenSource.Cancel();
         }
     }
 }
